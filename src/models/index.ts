53d2d4362b61b69d89b8c0dad7ca86c11ac20fcb
esteban-vm <estebanvm90@outlook.com>
2023-09-16 19:22:45 -0400

    added Rhinomorph

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,4 @@
-export { default as Enemy, Beetlemorph } from '@/enemy'
+export { default as Enemy, Beetlemorph, Rhinomorph } from '@/enemy'
 export { default as GameObject } from '@/game-object'
 export { default as Jets } from '@/jets'
 export { default as Player } from '@/player'
